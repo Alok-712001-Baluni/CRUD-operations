a32c4d9be3c76bdaef57780ab7c581ca9e192057
Disharna Das <67505920+disharnadas@users.noreply.github.com>
2022-05-16 19:16:55 +0530

    updated app.js
    some changes to the code.

--- server/app.js
+++ server/app.js
@@ -1,22 +1,22 @@
-require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
+
 require("./db/conn");
-const Schedule = require("./models/scheduleSchema");
-const cors = require("cors");
+
+
 const router = require("./routes/router");
 
 const port = 2000;
 
-app.use(cors());
+
 app.use(express.json());
 
 app.get("/",(req,res)=>{
     res.json("server start")
 })
 
-app.use(router);
+app.use('/api',router);
 
 app.listen(port, () => {
     console.log(`server is started at port number ${port}`);
