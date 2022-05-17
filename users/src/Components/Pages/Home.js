d96eca28c8438939bb467283c844ec03b1decc75
Disharna Das <67505920+disharnadas@users.noreply.github.com>
2022-05-17 18:11:15 +0530

    Update Home.js
    implemented viewing all  training data from database

--- users/src/Components/Pages/Home.js
+++ users/src/Components/Pages/Home.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
 import { NavLink } from "react-router-dom";
-import Schedule from "./Schedule";
-import "./Home.css";
-const Home = () => {
-  return (
-    <div className="mt-4">
+
+export default class PersonList extends React.Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:2000/api/getAll`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+
+  
+
+  render() {
+    return (
+
+
+
+      <div className="mt-4">
       <div className="container">
         <div className="schedule_btn mt-2">
           <NavLink className="btn btn-primary" to="/schedule"> Create Schedule</NavLink>
@@ -26,59 +44,41 @@ const Home = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Java</td>
-              <td>Subhash</td>
-              <td>23-05-2022</td>
-              <td>11:00</td>
-              <td>14:00</td>
-              <td>400</td>
-              <td>40</td>
-              <td>15</td>
+         
+      
+            
+            {
+          this.state.persons
+            .map(person =>
+              <tr key={person.id}>
+              <th scope="row">{person._id}</th>
+              <td>{person.course}</td>
+              <td>{person.trainer_name}</td>
+              <td>{person.date}</td>
+              <td>{person.starting_time}</td>
+              <td>{person.ending_time}</td>
+              <td>{person.fee_amount}</td>
+              <td>{person.disc_amount}</td>
+              <td>{person.fee_amount}</td>
               <td className="d-flex justify-content-between">
-                  <button className="btn btn-success"><i className="fa-regular fa-eye"></i></button>
+                  
                   <button className="btn btn-primary"><i className="fas fa-pen"></i></button>
                   <button className="btn btn-danger"><i className="fas fa-trash"></i></button>
               </td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Python</td>
-              <td>Rohan</td>
-              <td>25-05-2022</td>
-              <td>14:00</td>
-              <td>16:00</td>
-              <td>500</td>
-              <td>50</td>
-              <td>30</td>
-              <td className="d-flex justify-content-between">
-                  <button className="btn btn-success"><i class="fa-regular fa-eye"></i></button>
-                  <button className="btn btn-primary"><i className="fas fa-pen"></i></button>
-                  <button className="btn btn-danger"><i className="fas fa-trash"></i></button>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Data science</td>
-              <td>Vivek</td>
-              <td>19-05-2022</td>
-              <td>10:00</td>
-              <td>13:00</td>
-              <td>300</td>
-              <td>30</td>
-              <td>20</td>
-              <td className="d-flex justify-content-between">
-                  <button className="btn btn-success"><i class="fa-regular fa-eye"></i></button>
-                  <button className="btn btn-primary"><i className="fas fa-pen"></i></button>
-                  <button className="btn btn-danger"><i className="fas fa-trash"></i></button>
-              </td>
-            </tr>
+              </tr>
+            )
+        }
+           
+            
+            
           </tbody>
         </table>
       </div>
     </div>
-  );
-};
+      
+      
+       
 
-export default Home;
+    )
+  }
+}
